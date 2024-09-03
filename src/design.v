373e3b97cafeca0b77f16853677d03960a22a222
gmkapilan <160381275+gmkapilan@users.noreply.github.com>
2024-09-03 11:54:41 -0700

    Update design.v

--- src/design.v
+++ src/design.v
@@ -1,54 +1,31 @@
 // Code your design here
-module tt_um_ClockAlarm(
-  input wire clk,
-  input wire rst_n,
-  //Input time to the clock to preset
-  //input wire [7:0] preset_hours,
-  //input wire [7:0] preset_minutes, 
-  //input wire [7:0] preset_seconds, 
-  //Input at which Alarm should go off
-  input wire [1:0] alarm_hours,
-  input wire [3:0] alarm_minutes, 
-  input wire ena, 
- 
-  //Output time in the clock
-  output reg [1:0] hours,
-  output reg [1:0] minutes, 
-  output reg [1:0] seconds, 
-  output reg alarm
+module tt_um_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
+wire [5:0] minutes_out;
+tt_um_ClockAlarm my_alarm(
+  .clk(clk),
+  .rst_n(rst_n),
+  .alarm_hours(ui_in[4:0]),
+  .alarm_minutes({uio_in[2:0], ui_in[7:5]}), 
+  .ena(uio_in[3]), 
+  //output
+  .hours(uo_out[4:0]),
+  .minutes(minutes_out),
+  .alarm(uo_out[5])  
+);
 
-  always @(posedge clk or posedge rst_n) begin
-    if (rst_n) begin
-          hours <= 0;
-          minutes <= 0;
-          seconds <= 0;
-          alarm <= 1'b0;
-      end else begin
+assign uio_oe = 8'b1111_0000;
+assign uo_out[7:5] = minutes_out[2:0];
+assign uio_out[7:4] = minutes_out[5:3];
 
-          seconds <= seconds + 2'd1;
-
-        if (seconds == 2'd3) begin
-              seconds <= 2'd0;
-              minutes <= minutes + 3'd1;
-          end
-
-        if (minutes == 3'd7 && seconds == 2'd3) begin
-              minutes <= 3'd0;
-              hours <= hours + 2'd1;
-          end
-
-        if (hours == 2'd3 && minutes == 3'd7 && seconds == 2'd3) begin
-              hours <= 2'd0;
-          end
-
-
-        if ((hours == alarm_hours) && (minutes == alarm_minutes)) begin
-               alarm <= 1'b1; 
-          end else begin
-               alarm <= 1'b0; 
-          end
-      end
-  end
 endmodule
+
