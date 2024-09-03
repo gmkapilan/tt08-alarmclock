0257bca26b51d131d80a309ac60e7ca7a7744e28
gmkapilan <160381275+gmkapilan@users.noreply.github.com>
2024-09-02 21:03:51 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -17,14 +17,15 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+   reg [1:0] alarm_hours;
+   reg [2:0] alarm_minutes;
+   wire [1:0] hours;
+   wire [1:0] minutes;
+   wire [1:0] seconds;
+   wire alarm;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_ClockAlarm my_alarm (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -32,14 +33,15 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+    .clk(clk),
+     .rst_n(rst_n),
+     .seconds(seconds),
+     .minutes(minutes),
+     .hours(hours),
+     .alarm_minutes(alarm_minutes),
+     .alarm_hours(alarm_hours),
+     .ena(ana),
+     .alarm(alarm)
   );
 
 endmodule
