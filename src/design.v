e40b7c23b02c8aa2285f78952062191d1725c5c2
gmkapilan <160381275+gmkapilan@users.noreply.github.com>
2024-09-02 20:18:18 -0700

    Update design.v

--- src/design.v
+++ src/design.v
@@ -8,8 +8,8 @@ module tt_um_ClockAlarm(
   //input wire [7:0] preset_seconds, 
   //Input at which Alarm should go off
   input wire [1:0] alarm_hours,
-  input wire [1:0] alarm_minutes, 
-  input wire [1:0] ena, 
+  input wire [3:0] alarm_minutes, 
+  input wire ena, 
  
   //Output time in the clock
   output reg [1:0] hours,
@@ -31,20 +31,20 @@ module tt_um_ClockAlarm(
 
         if (seconds == 2'd3) begin
               seconds <= 2'd0;
-              minutes <= minutes + 2'd1;
+              minutes <= minutes + 3'd1;
           end
 
-        if (minutes == 2'd3 && seconds == 2'd3) begin
-              minutes <= 2'd0;
+        if (minutes == 3'd7 && seconds == 2'd3) begin
+              minutes <= 3'd0;
               hours <= hours + 2'd1;
           end
 
-        if (hours == 2'd3 && minutes == 2'd3 && seconds == 2'd3) begin
+        if (hours == 2'd3 && minutes == 3'd7 && seconds == 2'd3) begin
               hours <= 2'd0;
           end
 
 
-        if ((hours == alarm_hours) && (minutes == alarm_minutes) && (seconds == ena)) begin
+        if ((hours == alarm_hours) && (minutes == alarm_minutes)) begin
                alarm <= 1'b1; 
           end else begin
                alarm <= 1'b0; 
